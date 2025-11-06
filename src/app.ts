12083951253cf4925911a080558b401069abed7b
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-11-05 20:43:04 -0600

    Set up initial API documentation.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
+import setupSwagger from "../src/config/swagger";
 
 const app: Express = express();
 
 app.use(express.json());
+setupSwagger(app);
 
 app.get("/api/v1/health", (req, res) => {
     res.json({
