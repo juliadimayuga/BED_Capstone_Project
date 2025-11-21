0af7170fa967e6e8e336b04db2058ce7e7d7f11f
juliadimayuga <jdimayuga3@academic.rrc.ca>
2025-11-21 11:17:24 -0600

    Added authentication files.

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-
+import { getAuth, Auth } from "firebase-admin/auth";
 import * as serviceAccount from "../gymServiceAccountKey.json";
 
 // Initialize the Firebase app with the service account credentials
@@ -12,5 +12,6 @@ initializeApp({
 // Get a reference to the Firestore service
 // This creates a Firestore instance that you can use to interact with your database
 const db: Firestore = getFirestore();
+const auth: Auth = getAuth();
 
-export { db };
+export { db, auth };
